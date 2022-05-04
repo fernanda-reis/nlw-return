aae4157ddc11a7c1454c6a69a025d6345c097537
Fernanda <fernandasoares.reis1@gmail.com>
2022-05-04 19:53:09 -0300

    pags de inicio e conteudo

--- web/src/Widget.tsx
+++ web/src/Widget.tsx
@@ -1,17 +1,20 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 
 export function Widget(){
 
 
     return (
-        <Popover className="absolute bottom-4 right-4">
+        <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
             {/*mostra hello words se isWidgetOpen == true*/}
-            <Popover.Panel>Hello World</Popover.Panel>
+            <Popover.Panel>
+                <WidgetForm/>
+            </Popover.Panel>
 
             {/* group: agrupar elementos  */}
-            <Popover.Button className="bg-brand-500 rsounded-full px-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className='w-6 h-6'/>
 
                 {/* group-hover: hover no grupo de elementos */}
