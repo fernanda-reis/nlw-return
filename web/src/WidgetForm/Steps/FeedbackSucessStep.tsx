8c8eef8da8a7bcf20468d1f3f5701c9ae8db55b5
Fernanda <fernandasoares.reis1@gmail.com>
2022-05-05 11:08:37 -0300

    função de screenshot

--- web/src/WidgetForm/Steps/FeedbackSucessStep.tsx
+++ web/src/WidgetForm/Steps/FeedbackSucessStep.tsx
@@ -1,3 +1,34 @@
-export function FeedbackSucessStep(){
-    return ()
+import { CloseButton } from "../../CloseButton";
+import sucessImageUrl from '../../assets/success.svg'
+
+interface FeedbackSuccessProps{
+    onFeedbackRestart: () => void
+}
+
+export function FeedbackSucessStep(props: FeedbackSuccessProps){
+    return (
+        <>
+            <header>
+                <CloseButton/>
+            </header>
+
+            <div
+            className="flex flex-col items-center py-10 w-[304px]"
+            >
+                <img src={sucessImageUrl}/>
+
+                <span className="text-xl mt-8">
+                    Agradecemos o feedback!
+                </span>
+
+                <button
+                type="button"
+                className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                onClick={props.onFeedbackRestart}
+                >
+                    Quero enviar outro
+                </button>
+            </div>
+        </>
+    )
 }
--- web/src/WidgetForm/index.tsx
+++ web/src/WidgetForm/index.tsx
@@ -1,36 +1,36 @@
 import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
 export const feedbackTypes = {
-    BUG: {
-        title: 'Problema',
-        image: {
-            source: bugImageUrl,
-            alt: 'Imagem de um inseto'
-        }
-
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
     },
-    IDEA: {
-        title: 'Ideia',
-        image: {
-            source: ideaImageUrl,
-            alt: 'Imagem de uma lâmpada'
-        }
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
     },
-    OTHER: {
-        title: 'Outro',
-        image: {
-            source: thoughtImageUrl,
-            alt: 'Imagem de um balão de pensamento'
-        }
-    }
-}
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
 
 // Object.entries(feedbackTypes) =>
 
@@ -40,34 +40,38 @@ export const feedbackTypes = {
 //   ['OTHER', {...}]
 //  ]
 
-
-
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState< FeedbackType | null >(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
-    function handleRestartFeedback(){
-        setFeedbackType(null);
-    }
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false)
+  }
 
-    return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-3rem)] md:w-auto">
-        
-        {!feedbackType ? (
-            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-        ) : (
-            <FeedbackContentStep 
-            feedbackType={feedbackType}
-            onFeedbackRestart={handleRestartFeedback}
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-3rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSucessStep onFeedbackRestart={handleRestartFeedback}/>
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestart={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
             />
-        )}
+          )}
+        </>
+      )}
 
-
-        <footer className="text-xs text-neutral-400">
-            Feito com ❤ pela Rocketseat
-        </footer>
-
-        </div>
-    )
+      <footer className="text-xs text-neutral-400">
+        Feito com ❤ pela Rocketseat
+      </footer>
+    </div>
+  );
 }
